54296ac0369e5a84732d5325489dc9f1181bfade
frandy <>
2024-06-06 18:25:04 +0800

    add weather cubit , weather state, searcg page, setting page weather page

---
+++ lib/weather/view/weather_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/search/search.dart';
+import 'package:flutter_weather/settings/settings.dart';
+import 'package:flutter_weather/theme/theme.dart';
+import 'package:flutter_weather/weather/weather.dart';
+import 'package:weather_repository/weather_repository.dart';
+
+
